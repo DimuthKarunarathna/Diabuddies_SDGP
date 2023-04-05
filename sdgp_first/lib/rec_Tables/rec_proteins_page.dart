d32546cc14bbd4db10bcf5cb0c5c25ec4b883a1a
Thushara-Piyasekara <btgp2001@gmail.com>
2023-04-06 04:59:50 +0530

    bug in food rec page

--- sdgp_first/lib/rec_Tables/rec_proteins_page.dart
+++ sdgp_first/lib/rec_Tables/rec_proteins_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sdgp_first/food_rec_page.dart';
 
-
 class proteins extends StatefulWidget {
   const proteins({Key? key}) : super(key: key);
 
@@ -11,7 +10,6 @@ class proteins extends StatefulWidget {
 
 class _proteinssState extends State<proteins> {
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -21,48 +19,55 @@ class _proteinssState extends State<proteins> {
           body: ListView(children: <Widget>[
             const Center(
                 child: Text(
-                  'You Must Include In Your Diabetes Diet',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                )),
+              'You Must Include In Your Diabetes Diet',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            )),
             DataTable(
               columns: const [
-                DataColumn(label: Text(
-                    'Name',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
-                )),
-                DataColumn(label: Text(
-                    'Nutrients',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
-                )),
+                DataColumn(
+                    label: Text('Name',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text('Nutrients',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold))),
               ],
               rows: const [
                 DataRow(cells: [
                   DataCell(Text('Plant-based proteins')),
-                  DataCell(Text('Beans, beans, lentils, peas, edamame, soy nuts, and temper are all plant-based protein foods that provide quality protein, healthy fats and fiber.')),
+                  DataCell(Text(
+                      'Beans, beans, lentils, peas, edamame, soy nuts, and temper are all plant-based protein foods that provide quality protein, healthy fats and fiber.')),
                 ]),
                 DataRow(cells: [
                   DataCell(Text('Fish and seafood')),
-                  DataCell(Text('Fish high in omega-3 fatty acids, shellfish including clams, crabs, scallops, shrimp and oysters.')),
+                  DataCell(Text(
+                      'Fish high in omega-3 fatty acids, shellfish including clams, crabs, scallops, shrimp and oysters.')),
                 ]),
                 DataRow(cells: [
                   DataCell(Text('Poultry')),
-                  DataCell(Text('Poultry without skin has lower saturated fat and cholesterol.')),
+                  DataCell(Text(
+                      'Poultry without skin has lower saturated fat and cholesterol.')),
                 ]),
                 DataRow(cells: [
                   DataCell(Text('Cheese and eggs')),
-                  DataCell(Text('Reduced-fat cheese, cottage cheese, eggs, and whole eggs.')),
+                  DataCell(Text(
+                      'Reduced-fat cheese, cottage cheese, eggs, and whole eggs.')),
                 ]),
                 DataRow(cells: [
                   DataCell(Text('Beef, pork, veal and lamb')),
-                  DataCell(Text('Limit red meat and processed meats, choosing leanest options such as chuck, rib, rump roast, round, sirloin, cubed, flank, porterhouse, T-bone steak or tenderloin.')),
+                  DataCell(Text(
+                      'Limit red meat and processed meats, choosing leanest options such as chuck, rib, rump roast, round, sirloin, cubed, flank, porterhouse, T-bone steak or tenderloin.')),
                 ]),
                 DataRow(cells: [
                   DataCell(Text('Butter')),
-                  DataCell(Text('The key is to include butter in a diet with 5% of calories from carbohydrates, 20% from protein, and 75% from fat.')),
+                  DataCell(Text(
+                      'The key is to include butter in a diet with 5% of calories from carbohydrates, 20% from protein, and 75% from fat.')),
                 ]),
                 DataRow(cells: [
                   DataCell(Text('Milk')),
-                  DataCell(Text('Milk provides important nutrients for people with diabetes, such as calcium, which can help prevent osteoporosis.')),
+                  DataCell(Text(
+                      'Milk provides important nutrients for people with diabetes, such as calcium, which can help prevent osteoporosis.')),
                 ]),
               ],
             ),
@@ -70,7 +75,7 @@ class _proteinssState extends State<proteins> {
               top: 520, // Position from top of the screen
               left: 20, // Position from left of the screen
               child: Image.asset(
-                'img/protein_2.jpg',
+                'img/protein_2.jpeg',
                 width: 150,
                 height: 150,
                 fit: BoxFit.contain,
@@ -81,17 +86,15 @@ class _proteinssState extends State<proteins> {
               right: 20,
               child: ElevatedButton(
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (builder)=>const FoodRecPage()));
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (builder) => const FoodRecPage()));
                 },
                 child: const Text('Back'),
               ),
             ),
-          ])
-      ),
+          ])),
     );
   }
 }
-
-
-
-
